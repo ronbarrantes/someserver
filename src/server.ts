5777c084bebcee93c6142b70c39e163bfee1f293
ronbarrantes <ronbarrantes@gmail.com>
2020-09-12 23:45:46 -0700

    Start server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("hello")
+import * as express from "express"
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.send('world')
+})
+
+app.listen(PORT, () => console.log(`Listening ${PORT}`))
